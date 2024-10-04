91752f24203da88ec7922d9be5d01644c121d85d
Nilesh Kumar <nileshandnilesh120@gmail.com>
2024-10-05 00:00:20 +0530

    just basic setup p1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
 import { Button } from "./components/ui/button";
+import { ThemeToggle } from "./components/mode-toggle";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Button>Hello</Button>
+      <div className="h-screen w-screen bg-blue-200">
+        <Routes>
+          <Route path="/" element={""} />
+        </Routes>
+        <Button>Hello</Button>
+        <Button>
+          <ThemeToggle />
+        </Button>
+      </div>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { RecoilRoot } from "recoil";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RecoilRoot>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </RecoilRoot>
+    </ThemeProvider>
+  </StrictMode>
+);
